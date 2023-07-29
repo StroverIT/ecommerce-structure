41cd5d7b51b7d1054eac784f15e4a6698f219298
StR0vErBG <xcreaw1@abv.bg>
2023-07-29 08:58:19 +0200

    adding global context and comments

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,18 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+// ! ---- This is fonts ---- !
+// import { Inter } from 'next/font/google'
+
+// const inter = Inter({ subsets: ['latin'] })
+// ! --- End of fonts ---- !
+
+// --- Layout
+import Nav from "@/components/Layout/Nav"
+import Footer from "@/components/Layout/Footer"
+
+// --- Global Context
+import {GlobalContextProvider} from "@/context/Global"
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +26,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+
+      <body  /* className={inter.className} */ className='bg-color' >
+      <GlobalContextProvider>
+
+        <Nav />
+        {children}
+        <Footer />
+        </GlobalContextProvider>
+
+      </body>
     </html>
   )
 }
