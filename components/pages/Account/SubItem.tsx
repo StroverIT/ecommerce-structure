87aaae6d23645b5ca9b02cab2c35dcc3c244e483
emil <emil@mclimate.eu>
2023-10-06 16:46:36 +0300

    chaning spacing

--- components/pages/Account/SubItem.tsx
+++ components/pages/Account/SubItem.tsx
@@ -11,17 +11,17 @@ function Item({ text, isLast, link }: any) {
     const [isActive, setActive] = useState(false)
   
     const setOff = () => {
-
       if(pathname == link) return
-
       setActive(false)
     }
+    
     useEffect(()=>{
       
       if(pathname == link) setActive(true)
       else setActive(false)
 
     },[pathname])
+
     return <Link href={link}>
       <li className={`relative cursor-pointer ${isLast && "-mb-3"}`} onMouseEnter={() => setActive(true)} onMouseLeave={() => setOff()}>
         <div className="bg-slate-300 h-[1px] absolute w-6 top-1/2 -translate-y-1/2 -left-4">
