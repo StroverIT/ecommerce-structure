7aaffbcd410b54f4e4c8f468a1d6ee3f124e6afc
emil <emil@mclimate.eu>
2023-10-09 20:33:48 +0300

    Formatting for readability

--- components/pages/Account/Menu.tsx
+++ components/pages/Account/Menu.tsx
@@ -1,38 +1,40 @@
-"use client"
+"use client";
 import { useState, useRef, useEffect } from "react";
-
-
 
 import SubMenu from "./SubMenu";
 
 import MenuItem from "./MenuItem";
 import { usePathname } from "next/navigation";
 
-
 const Menu = (props: any) => {
-  const pathname = usePathname()  
-  const splitLink = props.link.split("/")
-  const destructedLink = `/${splitLink[1]}/${splitLink[2]}/${splitLink[3]}`
-  
-  const condition = pathname.includes( destructedLink)
+  const pathname = usePathname();
+  const splitLink = props.link.split("/");
+  const destructedLink = `/${splitLink[1]}/${splitLink[2]}/${splitLink[3]}`;
 
-  const [subMenuState, subMenuSetState] = useState(condition ? true : false)
-    
- 
+  const condition = pathname.includes(destructedLink);
+
+  const [subMenuState, subMenuSetState] = useState(condition ? true : false);
+
   return (
     <>
+      <MenuItem
+        submenu={props.submenu}
+        link={props.link}
+        name={props.name}
+        subMenuSetState={subMenuSetState}
+        subMenuState={subMenuState}
+        condition={condition}
+      />
 
-      <MenuItem submenu={props.submenu} 
-      link={props.link} name={props.name}
-       subMenuSetState={subMenuSetState}
-       subMenuState={subMenuState}
-       condition={condition}
-       />
-
-      {props.submenu && <SubMenu submenu={props.submenu} link={props.link} subMenuState={subMenuState}/>}
+      {props.submenu && (
+        <SubMenu
+          submenu={props.submenu}
+          link={props.link}
+          subMenuState={subMenuState}
+        />
+      )}
     </>
-
   );
 };
 
-export default Menu
+export default Menu;
